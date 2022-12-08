01ecc147910401e0a4aa9d2b53a9e01d2dd4d4d9
Lauri Saarela <lauri.saarela@gofore.com>
2022-12-08 10:51:01 +0200

    wrap all code in self executing async function, await api response

--- index.js
+++ index.js
@@ -2,46 +2,48 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
-try {
-  const org = core.getInput('pulumi-organization');
-  console.log(`Org ${org}!`);
-  const project = core.getInput('pulumi-project');
-  console.log(`Project ${project}!`);
-  const stack = core.getInput('pulumi-stack');
-  console.log(`Stack ${stack}!`);
-  const resourceName = core.getInput('pulumi-resource');
-  console.log(`Resource ${resourceName}!`);
-  const accessToken = core.getInput('pulumi-access-token');
-  // console.log(`token ${accessToken}!`);
+(async () => {
+  try {
+    const org = core.getInput('pulumi-organization');
+    console.log(`Org ${org}!`);
+    const project = core.getInput('pulumi-project');
+    console.log(`Project ${project}!`);
+    const stack = core.getInput('pulumi-stack');
+    console.log(`Stack ${stack}!`);
+    const resourceName = core.getInput('pulumi-resource');
+    console.log(`Resource ${resourceName}!`);
+    const accessToken = core.getInput('pulumi-access-token');
+    // console.log(`token ${accessToken}!`);
 
-  const url = `https://api.pulumi.com/api/stacks/${org}/${project}/${stack}/export`;
+    const url = `https://api.pulumi.com/api/stacks/${org}/${project}/${stack}/export`;
 
-  const response = axios({
-    method: 'get',
-    url,
-    headers: {
-      Accept: 'application/vnd.pulumi+8',
-      'Content-Type': 'application/json',
-      Authorization: `token ${accessToken}`,
-    },
-  });
+    const response = await axios({
+      method: 'get',
+      url,
+      headers: {
+        Accept: 'application/vnd.pulumi+8',
+        'Content-Type': 'application/json',
+        Authorization: `token ${accessToken}`,
+      },
+    });
 
-  console.log(response);
+    console.log(response);
 
-  let resourceOutput = '';
+    let resourceOutput = '';
 
-  const resourceObject = response.data.deployment?.resources?.find(
-    r => r.type === 'pulumi:pulumi:Stack'
-  );
+    const resourceObject = response.data.deployment?.resources?.find(
+      r => r.type === 'pulumi:pulumi:Stack'
+    );
 
-  if (resourceObject) {
-    resourceOutput = resourceObject.outputs[`${resourceName}`] || '';
+    if (resourceObject) {
+      resourceOutput = resourceObject.outputs[`${resourceName}`] || '';
+    }
+
+    core.setOutput('resource-output', response.data?.deployment?.resources);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  core.setOutput('resource-output', response.data?.deployment?.resources);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+})();
